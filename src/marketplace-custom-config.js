789ecedd51fd642c658e5ef02a68003dae431fef
Ali Hassan Mirza <ali.hassan.mirzaa+100@gmail.com>
2021-06-19 08:40:01 -0400

    Chanes for car categories and struture.

--- src/marketplace-custom-config.js
+++ src/marketplace-custom-config.js
@@ -85,9 +85,9 @@ export const filters = [
       // Note: label is not added through the translation files
       // to make filter customizations a bit easier.
       options: [
-        { key: 'smoke', label: 'Smoke' },
-        { key: 'electric', label: 'Electric' },
-        { key: 'wood', label: 'Wood' },
+        { key: 'hybrid', label: 'Hybrid' },
+        { key: 'diesel', label: 'Diesel' },
+        { key: 'petrol', label: 'Petrol' },
         { key: 'other', label: 'Other' },
       ],
     },
@@ -109,36 +109,32 @@ export const filters = [
       // to make filter customizations a bit easier.
       options: [
         {
-          key: 'towels',
-          label: 'Towels',
+          key: 'ac',
+          label: 'AC',
         },
         {
-          key: 'bathroom',
-          label: 'Bathroom',
+          key: 'air_bags',
+          label: 'Air Bags',
         },
         {
-          key: 'swimming_pool',
-          label: 'Swimming pool',
+          key: 'multimedia',
+          label: 'Multimedia',
         },
         {
-          key: 'own_drinks',
-          label: 'Own drinks allowed',
+          key: 'sunroof',
+          label: 'Sunroof',
         },
         {
-          key: 'jacuzzi',
-          label: 'Jacuzzi',
+          key: 'child_lock',
+          label: 'Child Lock',
         },
         {
-          key: 'audiovisual_entertainment',
-          label: 'Audiovisual entertainment',
+          key: 'back_camera',
+          label: 'Back Camera',
         },
         {
-          key: 'barbeque',
-          label: 'Barbeque',
-        },
-        {
-          key: 'own_food_allowed',
-          label: 'Own food allowed',
+          key: 'led',
+          label: 'LED Lights',
         },
       ],
     },
